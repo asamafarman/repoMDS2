39b79182a0cf2a0e07d76e4bd9d569b0fcbb6f34
acb083 <alexandbcn@hotmail.com>
2022-03-21 14:12:43 +0100

    Vista principal

--- proyectomds2/frontend/src/vista-principal.js
+++ proyectomds2/frontend/src/vista-principal.js
@@ -1,24 +1,46 @@
 import {html, PolymerElement} from '@polymer/polymer/polymer-element.js';
+import '@vaadin/vaadin-ordered-layout/src/vaadin-vertical-layout.js';
+import '@vaadin/vaadin-text-field/src/vaadin-text-field.js';
+import '@vaadin/vaadin-ordered-layout/src/vaadin-horizontal-layout.js';
+import '@vaadin/vaadin-button/src/vaadin-button.js';
 
-/**
- * `vista-principal`
- *
- * VistaPrincipal element.
- *
- * @customElement
- * @polymer
- */
 class VistaPrincipal extends PolymerElement {
 
     static get template() {
         return html`
-            <style include="shared-styles">
+<style include="shared-styles">
                 :host {
                     display: block;
                     height: 100%;
                 }
             </style>
-        `;
+<vaadin-vertical-layout style="width: 100%; height: 100%;" id="vaadinVerticalLayout">
+ <vaadin-text-field id="vaadinTextField" style="width: 100%; height: 100%;" placeholder="Correo electronico"></vaadin-text-field>
+ <vaadin-text-field id="vaadinTextField1" style="width: 100%; height: 100%;" placeholder="Contraseña"></vaadin-text-field>
+ <vaadin-horizontal-layout theme="spacing" id="vaadinHorizontalLayout" style="width: 100%; height: 100%;">
+  <vaadin-button id="vaadinButton" style="width: 100%; height: 100%;">
+   Iniciar Sesion
+  </vaadin-button>
+  <vaadin-button id="vaadinButton1" style="width: 100%; height: 100%;">
+   Olvidé Contraseña
+  </vaadin-button>
+ </vaadin-horizontal-layout>
+ <vaadin-horizontal-layout theme="spacing" id="vaadinHorizontalLayout1" style="width: 100%; height: 100%;">
+  <vaadin-button id="vaadinButton2" style="width: 100%; height: 100%;">
+   Facebook
+  </vaadin-button>
+  <vaadin-button id="vaadinButton3" style="width: 100%; height: 100%;">
+   Google 
+  </vaadin-button>
+  <vaadin-button id="vaadinButton4" style="height: 100%; width: 100%;">
+   Apple Id
+  </vaadin-button>
+ </vaadin-horizontal-layout>
+ <vaadin-button id="vaadinButton5" style="width: 100%; height: 100%;">
+  Registrarse
+ </vaadin-button>
+</vaadin-vertical-layout>
+`;
     }
 
     static get is() {
--- proyectomds2/src/main/java/vistas/VistaPrincipal.java
+++ proyectomds2/src/main/java/vistas/VistaPrincipal.java
@@ -4,6 +4,11 @@ import com.vaadin.flow.templatemodel.TemplateModel;
 import com.vaadin.flow.component.Tag;
 import com.vaadin.flow.component.dependency.JsModule;
 import com.vaadin.flow.component.polymertemplate.PolymerTemplate;
+import com.vaadin.flow.dom.Element;
+import com.vaadin.flow.component.template.Id;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.textfield.TextField;
 
 /**
  * A Designer generated component for the vista-principal template.
@@ -15,7 +20,31 @@ import com.vaadin.flow.component.polymertemplate.PolymerTemplate;
 @JsModule("./src/vista-principal.js")
 public class VistaPrincipal extends PolymerTemplate<VistaPrincipal.VistaPrincipalModel> {
 
-    /**
+    @Id("vaadinVerticalLayout1")
+	private Element vaadinVerticalLayout1;
+	@Id("vaadinButton")
+	private Button vaadinButton;
+	@Id("vaadinButton1")
+	private Button vaadinButton1;
+	@Id("vaadinHorizontalLayout")
+	private HorizontalLayout vaadinHorizontalLayout;
+	@Id("vaadinButton2")
+	private Button vaadinButton2;
+	@Id("vaadinButton3")
+	private Button vaadinButton3;
+	@Id("vaadinButton4")
+	private Button vaadinButton4;
+	@Id("vaadinButton5")
+	private Button vaadinButton5;
+	@Id("vaadinVerticalLayout")
+	private Element vaadinVerticalLayout;
+	@Id("vaadinTextField")
+	private TextField vaadinTextField;
+	@Id("vaadinTextField1")
+	private TextField vaadinTextField1;
+	@Id("vaadinHorizontalLayout1")
+	private HorizontalLayout vaadinHorizontalLayout1;
+	/**
      * Creates a new VistaPrincipal.
      */
     public VistaPrincipal() {
