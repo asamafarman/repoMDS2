da041aa2f4ffdac5d9b2ac818b9e799f3b4d3ae9
Asama Muhammad Farman <af053@inlumine.ual.es>
2022-03-26 20:43:48 +0100

    Modificacion en vista-logo, se añade imagen

--- proyectomds2/frontend/src/vista-logo.js
+++ proyectomds2/frontend/src/vista-logo.js
@@ -12,7 +12,7 @@ class VistaLogo extends PolymerElement {
                 }
             </style>
 <vaadin-vertical-layout style="width: 100%; height: 100%; padding: var(--lumo-space-l);" id="vertical_logo">
- <img id="img_logo" style="width: 50%; height: 30%; flex-shrink: 0; align-self: flex-start;" src="https://imgur.com/a/K94oq2H">
+ <img id="img_logo" style="width: 20%; height: 30%; flex-shrink: 0; align-self: flex-start;" src="https://play-lh.googleusercontent.com/7N4p6FtJ1jsI4vSEYY40HhHcYXTQKV5nEXyle5AMj_FMxU-UqsN_dxFL80kpM81WCQ">
 </vaadin-vertical-layout>
 `;
     }
